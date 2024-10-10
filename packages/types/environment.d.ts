1d5f3059199701e54be14233a2b060b303eff758
Ismailmehri <ismail.mehri@gmail.com>
2024-10-10 19:20:45 +0200

    Add google TAG

--- packages/types/environment.d.ts
+++ packages/types/environment.d.ts
@@ -58,6 +58,7 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_APP_NAME: string | "Cal";
     readonly NEXT_PUBLIC_SUPPORT_MAIL_ADDRESS: string | "help@cal.com";
     readonly NEXT_PUBLIC_COMPANY_NAME: string | "Cal.com, Inc.";
+    readonly NEXT_PUBLIC_GTM_ID: string | "GTM-NLVDRV7V";
     /**
      *  "strict" -> Strict CSP
      *  "non-strict" -> Strict CSP except the usage of unsafe-inline for `style-src`
